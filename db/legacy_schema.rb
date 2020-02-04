59ba85696654794bf5d96463a57f637b479e2b0b
Mark H. Wilkinson <mhw@dangerous-techniques.com>
2020-02-04 13:18:23 +0000

    Add view to legacy database

--- db/legacy_schema.rb
+++ db/legacy_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_131522) do
+ActiveRecord::Schema.define(version: 2020_02_04_131648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,9 @@ ActiveRecord::Schema.define(version: 2020_02_04_131522) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+
+  create_view "thing_titles", sql_definition: <<-SQL
+      SELECT things.title
+     FROM things;
+  SQL
 end
